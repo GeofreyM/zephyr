6252bc7dd0b2d82bae6601d9f03536a14da03df2
Geofrey <g.marcel@trilys.com>
2024-10-31 09:36:38 +0100

    RXAFILTEN must be set before UCPDEN

--- drivers/usb_c/tcpc/ucpd_stm32.c
+++ drivers/usb_c/tcpc/ucpd_stm32.c
@@ -1356,6 +1356,10 @@ static int ucpd_init(const struct device *dev)
 			LL_UCPD_ORDERSET_HARDRST;
 		stm32_reg_write(&config->ucpd_port->CFG1, cfg1);
 
+#if defined(STM32H5) || defined(STM32U5) || defined(STM32H7RS)
+		LL_UCPD_RxAnalogFilterEnable(config->ucpd_port);
+#endif
+
 		/* Enable UCPD port */
 		LL_UCPD_Enable(config->ucpd_port);
 
@@ -1369,10 +1373,6 @@ static int ucpd_init(const struct device *dev)
 			 */
 			dead_battery(dev, false);
 		}
-
-#if defined(STM32H5) || defined(STM32U5) || defined(STM32H7RS)
-		LL_UCPD_RxAnalogFilterEnable(config->ucpd_port);
-#endif
 
 		/* Initialize the isr */
 		ucpd_isr_init(dev);
